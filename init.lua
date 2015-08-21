97cbfde5754ef0016074a0038bdecae35a6f87b2
Tim Reddehase <tim.reddehase@xing.com>
2015-08-21 16:59:20 +0200

    adjust config for custom base-meta (hit/hits).

--- init.lua
+++ init.lua
@@ -15,8 +15,11 @@ local tink_sound  = sound.get_byname("Tink") -- Not actually used, just as a nic
                                              -- More sounds in /System/Library/Sounds
 
 -- Set up hotkey combinations
-local mash      = {"cmd", "alt", "ctrl"}
-local mashshift = {"cmd", "alt", "shift"}
+-- local mash      = {"cmd", "alt", "ctrl"}
+-- local mashshift = {"cmd", "alt", "shift"}
+local hit = {"cmd", "ctrl"}
+local hits = {"cmd", "ctrl", "shift"}
+
 -- Set grid size.
 grid.GRIDWIDTH  = 12
 grid.GRIDHEIGHT = 12
@@ -28,50 +31,70 @@ local function opendictionary()
   application.launchorfocus("Dictionary")
 end
 
-hotkey.bind(mash, 'D', opendictionary)
+-- Maximize and Minimize the current window
+hotkey.bind(hit, 'M', grid.maximize_window)
+hotkey.bind(hits, 'M', function() window.focusedwindow():minimize() end)
 
-hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
-hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
+-- Spotify interaction
+hotkey.bind(hits, 'space', spotify.play)
+-- hotkey.bind(hits, 'K', spotify.play)
+-- hotkey.bind(hits, 'J', spotify.pause)
+hotkey.bind(hits, 'K', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume + 5) end)
+hotkey.bind(hits, 'J', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume - 5) end)
+hotkey.bind(hits, 'L', spotify.next)
+hotkey.bind(hits, 'H', spotify.previous)
 
-hotkey.bind(mash,      '=', function() grid.adjustwidth(1) end)
-hotkey.bind(mash,      '-', function() grid.adjustwidth(-1) end)
-hotkey.bind(mashshift, '=', function() grid.adjustheight(1) end)
-hotkey.bind(mashshift, '-', function() grid.adjustheight(-1) end)
+-- Speaker Volume
+-- hotkey.bind(hit, 'k', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume + 5) end)
+-- hotkey.bind(hit, 'j', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume - 5) end)
 
-hotkey.bind(mashshift, 'left',  function() window.focusedwindow():focuswindow_west() end)
-hotkey.bind(mashshift, 'right', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'up',    function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'down',  function() window.focusedwindow():focuswindow_south() end)
+hotkey.bind(hits, 'left',  function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(hits, 'right', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(hits, 'up',    function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(hits, 'down',  function() window.focusedwindow():focuswindow_south() end)
 
-hotkey.bind(mash,      'M', grid.maximize_window)
-hotkey.bind(mashshift, 'M', function() window.focusedwindow():minimize() end)
+-- hotkey.bind(mash, 'D', opendictionary)
 
-hotkey.bind(mash,      'F', function() window.focusedwindow():setfullscreen(true) end)
-hotkey.bind(mashshift, 'F', function() window.focusedwindow():setfullscreen(false) end)
+-- hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
+-- hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
 
-hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
-hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+-- hotkey.bind(mash,      '=', function() grid.adjustwidth(1) end)
+-- hotkey.bind(mash,      '-', function() grid.adjustwidth(-1) end)
+-- hotkey.bind(mashshift, '=', function() grid.adjustheight(1) end)
+-- hotkey.bind(mashshift, '-', function() grid.adjustheight(-1) end)
 
-hotkey.bind(mash, 'J', grid.pushwindow_down)
-hotkey.bind(mash, 'K', grid.pushwindow_up)
-hotkey.bind(mash, 'H', grid.pushwindow_left)
-hotkey.bind(mash, 'L', grid.pushwindow_right)
+-- hotkey.bind(mashshift, 'left',  function() window.focusedwindow():focuswindow_west() end)
+-- hotkey.bind(mashshift, 'right', function() window.focusedwindow():focuswindow_east() end)
+-- hotkey.bind(mashshift, 'up',    function() window.focusedwindow():focuswindow_north() end)
+-- hotkey.bind(mashshift, 'down',  function() window.focusedwindow():focuswindow_south() end)
 
-hotkey.bind(mash, 'U', grid.resizewindow_taller)
-hotkey.bind(mash, 'O', grid.resizewindow_wider)
-hotkey.bind(mash, 'I', grid.resizewindow_thinner)
-hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
+-- hotkey.bind(mash,      'M', grid.maximize_window)
+-- hotkey.bind(mashshift, 'M', function() window.focusedwindow():minimize() end)
 
-hotkey.bind(mashshift, 'space', spotify.displayCurrentTrack)
-hotkey.bind(mashshift, 'P',     spotify.play)
-hotkey.bind(mashshift, 'O',     spotify.pause)
-hotkey.bind(mashshift, 'N',     spotify.next)
-hotkey.bind(mashshift, 'I',     spotify.previous)
+-- hotkey.bind(mash,      'F', function() window.focusedwindow():setfullscreen(true) end)
+-- hotkey.bind(mashshift, 'F', function() window.focusedwindow():setfullscreen(false) end)
 
-hotkey.bind(mashshift, 'T', function() alert.show(os.date("%A %b %d, %Y - %I:%M%p"), 4) end)
+-- hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+-- hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
 
-hotkey.bind(mashshift, ']', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume + 5) end)
-hotkey.bind(mashshift, '[', function() audiodevice.defaultoutputdevice():setvolume(audiodevice.current().volume - 5) end)
+-- hotkey.bind(mash, 'J', grid.pushwindow_down)
+-- hotkey.bind(mash, 'K', grid.pushwindow_up)
+-- hotkey.bind(mash, 'H', grid.pushwindow_left)
+-- hotkey.bind(mash, 'L', grid.pushwindow_right)
+
+-- hotkey.bind(mash, 'U', grid.resizewindow_taller)
+-- hotkey.bind(mash, 'O', grid.resizewindow_wider)
+-- hotkey.bind(mash, 'I', grid.resizewindow_thinner)
+-- hotkey.bind(mash, 'Y', grid.resizewindow_shorter)
+
+-- hotkey.bind(mashshift, 'space', spotify.displayCurrentTrack)
+-- hotkey.bind(mashshift, 'P',     spotify.play)
+-- hotkey.bind(mashshift, 'O',     spotify.pause)
+-- hotkey.bind(mashshift, 'N',     spotify.next)
+-- hotkey.bind(mashshift, 'I',     spotify.previous)
+
+-- hotkey.bind(mashshift, 'T', function() alert.show(os.date("%A %b %d, %Y - %I:%M%p"), 4) end)
+
 
 alert_sound:play()
-alert.show("Mjolnir, at your service.", 3)
+alert.show("Mjolnir now rests safely in your hand", 3)
